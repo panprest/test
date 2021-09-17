2b2bb01345133f3008456bfdf727b001f3d815e4
panji.prasetyo <panji.prasetyo@gli.id>
2021-09-17 23:40:23 +0700

    initial commit phase 2

--- src/main/java/com/example/test/Model.java
+++
@@ -1,6 +0,0 @@
-package com.example.test;
-
-public class Model {
-    String id;
-    String name;
-}
--- src/main/java/com/example/test/Test.java
+++
@@ -1,8 +0,0 @@
-package com.example.test;
-
-public class Test {
-    private String id;
-    private String name;
-    private String phone;
-    private String birthday;
-}
